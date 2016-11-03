a2f51691c5dc3bec9dc80051fac6c449ee89134f
Prasanna <vprasana@thoughtworks.com>
2016-11-03 23:14:05 +0530

    Setup vim in the remote machine

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -1,3 +1,9 @@
 # Just copy the settings in the remote machine
 
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install ctags
+apt install exuberant-ctags
+
 cp ./dot-files/.* ~
